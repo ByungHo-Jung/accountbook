90150e4ee7cc5dfb384819655dd32dc457c33482
Anchangmin <56183155+minicomet@users.noreply.github.com>
2021-06-11 19:51:49 +0900

    1
    1

--- app/src/main/java/com/example/accountbook/MainActivity.java
+++ app/src/main/java/com/example/accountbook/MainActivity.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
